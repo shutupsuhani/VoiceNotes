8ae1cf5c31e404ec3eec00101e181d379038818d
Suhani Sahu <sahusuhani14@gmail.com>
2025-08-30 23:29:53 +0530

    cors error

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();   
 
 // Middleware
-app.use(cors());
+
 app.use(cors({
   origin: ["https://voice-notes-bysuhani.vercel.app","http://localhost:5173"], 
   credentials: true, // required for cookies
