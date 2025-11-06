4c712da0d50978ae3d5f7b67ff0b410defe6b267
Suhani Sahu <sahusuhani14@gmail.com>
2025-11-06 08:47:28 +0530

    changed gemini model

--- Backend/routes/noteRoute.js
+++ Backend/routes/noteRoute.js
@@ -43,7 +43,7 @@ router.post("/summary/:id", authenticate, async (req, res) => {
     if (!note) return res.status(404).json({ message: "Note not found" });
 
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
       {
         method: "POST",
         headers: {
